aff2725af259b663d93967552d56edceea2c492d
Green <shren0812@gmail.com>
2022-05-27 19:53:50 +0900

    first commit: `4:6 Query Resolvers`

---
+++ server.js
@@ -0,0 +1,32 @@
+import {ApolloServer, gql} from "apollo-server";
+
+const typeDefs = gql`
+    type User {
+        id: ID
+        username: String
+    }
+    type Tweet {
+        id: ID
+        text: String
+        author: User
+    }
+    type Query {  
+        allTweets: [Tweet]!  
+        tweet(id: ID): Tweet
+    }
+    type Mutation {
+        postTweet(text: String, userId: ID): Tweet
+        deleteTweet(id: ID): Boolean
+    }
+`
+
+const resolvers = {}
+
+const server = new ApolloServer({
+    typeDefs
+})
+
+server.listen().then(({url}) => {
+    console.log(`Server ready at ${url}`)
+})
+c
