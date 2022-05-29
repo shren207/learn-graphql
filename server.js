50b285e6e3d58a620e70d21f0f23f3a572e9d395
shren207 <shren0812@gmail.com>
2022-05-29 18:39:10 +0900

    #4.7 : Mutation Resolver

--- server.js
+++ server.js
@@ -1,22 +1,14 @@
 import {ApolloServer, gql} from "apollo-server";
 
 const tweets = [
-    {
-        id: "1",
-        text: "Hello world",
-        author: {
-            id: "1",
-            name: "Hira",
-        }
-    },
-    {
-        id: "2",
-        text: "Hello world 2",
-        author: {
-            id: "2",
-            name: "Jun99"
-        }
-    }
+  {
+    id: "1",
+    text: "Hello world",
+  },
+  {
+    id: "2",
+    text: "Hello world 2",
+  }
 ]
 
 const typeDefs = gql`
@@ -29,40 +21,47 @@ const typeDefs = gql`
         text: String
         author: User
     }
-    type Query {  
-        allTweets: [Tweet]!  
+    type Query {
+        allTweets: [Tweet]!
         tweet(id: ID): Tweet
         ping: String!
     }
     type Mutation {
-        postTweet(text: String, userId: ID): Tweet
+        postTweet(text: String, userId: ID): Tweet!
         deleteTweet(id: ID): Boolean
     }
 `
 
 const resolvers = {
-    Query: {
-        allTweets() {
-            return tweets
-        },
-        tweet(root, {id}) { // id === "1"
-            console.log(typeof id)
-            return tweets.find(tweet => tweet.id === id)
-        },
-        ping() {
-            return "pong"
-        }
+  Query: {
+    allTweets() {
+      return tweets
     },
-    Mutation: {
-
+    tweet(root, {id}) { // id === "1"
+      console.log(typeof id)
+      return tweets.find(tweet => tweet.id === id)
+    },
+    ping() {
+      return "pong"
     }
+  },
+  Mutation: {
+    postTweet(_, {text, userId}) {
+      const tweet = {
+        id: tweets.length + 1,
+        text,
+      }
+      tweets.push(tweet);
+      return tweet;
+    }
+  }
 }
 
 const server = new ApolloServer({
-    typeDefs, resolvers
+  typeDefs, resolvers
 })
 
 server.listen().then(({url}) => {
-    console.log(`Server ready at ${url}`)
+  console.log(`Server ready at ${url}`)
 })
 
