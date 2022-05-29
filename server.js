02849f13fc285f4f157cd131f1f9389f75b4c953
shren207 <shren0812@gmail.com>
2022-05-29 18:02:58 +0900

    #4.6 : Query Resolvers

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
 import {ApolloServer, gql} from "apollo-server";
 
+const tweets = [
+    {
+        id: "1",
+        text: "Hello world",
+        author: {
+            id: "1",
+            name: "Hira",
+        }
+    },
+    {
+        id: "2",
+        text: "Hello world 2",
+        author: {
+            id: "2",
+            name: "Jun99"
+        }
+    }
+]
+
 const typeDefs = gql`
     type User {
         id: ID
@@ -13,6 +32,7 @@ const typeDefs = gql`
     type Query {  
         allTweets: [Tweet]!  
         tweet(id: ID): Tweet
+        ping: String!
     }
     type Mutation {
         postTweet(text: String, userId: ID): Tweet
@@ -20,13 +40,29 @@ const typeDefs = gql`
     }
 `
 
-const resolvers = {}
+const resolvers = {
+    Query: {
+        allTweets() {
+            return tweets
+        },
+        tweet(root, {id}) { // id === "1"
+            console.log(typeof id)
+            return tweets.find(tweet => tweet.id === id)
+        },
+        ping() {
+            return "pong"
+        }
+    },
+    Mutation: {
+
+    }
+}
 
 const server = new ApolloServer({
-    typeDefs
+    typeDefs, resolvers
 })
 
 server.listen().then(({url}) => {
     console.log(`Server ready at ${url}`)
 })
-c
+
